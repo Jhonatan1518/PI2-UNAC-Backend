ee017c98f37ccb5027206fd135adbf8df641f0a0
Jhonatan Zapata <jhonatan.zapatah@unac.edu.co>
2023-11-13 16:43:12 -0500

    changes database
    Co-authored-by: Jhonatan1518 <Jhonatan1518@users.noreply.github.com>

--- src/database.js
+++ src/database.js
@@ -1,11 +1,10 @@
-
-import mongoose from 'mongoose';
-
-export const connectDB = async () => {
-  try {
-    const db = await mongoose.connect('mongodb+srv://jhonatanzapatah:0rlnQwEVbp8M1EYP@cluster0.eg38mhn.mongodb.net/');
-    console.log('DB connected to', db.connection.name);
-  } catch (error) {
-    console.error(error);
-  }
-};
+import mongoose from 'mongoose' 
+ 
+(async()=>{ 
+    try{ 
+        const db= await mongoose.connect('mongodb+srv://jhonatanzapatah:0rlnQwEVbp8M1EYP@cluster0.eg38mhn.mongodb.net/'); 
+        console.log('DB connected to', db.connection.name); 
+    }catch (error){ 
+        console.error(error); 
+    } 
+})();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import app from './src/app';
-import './src/database';
+import app from './app';
+import './database';
 
 
 app.listen(3000)
